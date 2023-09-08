97e593c2bdefbfefd35c2c9ccaef914517d60fe7
taoqf <tao_qiufeng@126.com>
2023-09-08 15:08:05 +0800

    fix: block text elements #242

--- test/tests/issues/242.js
+++ test/tests/issues/242.js
@@ -1,6 +1,6 @@
 const { parse } = require('@test/test-target');
 
-describe('issue 242', function () {
+describe.only('issue 242', function () {
 	it(`a.rawAttrs returns 'href="/" rel="home"' but a.getAttribute("href') returns undefined`, function () {
 		const html = `<div><a href="/" rel="home">Git Hub</a></div>`;
 		const root = parse(html);
@@ -9,5 +9,21 @@ describe('issue 242', function () {
 		const [a] = links;
 		a.rawAttrs.should.eql('href="/" rel="home"');
 		a.getAttribute('href').should.eql('/');
+	});
+	it(`get code`, function () {
+		const html = `<pre>
+  <code>test</code>
+</pre>`;
+		const root = parse(html, {
+			blockTextElements: {
+				script: true,
+				noscript: true,
+				style: true,
+				pre: false
+			}
+		});
+		const list = root.getElementsByTagName("code");
+		const [code] = list;
+		code.text.should.eql('test');
 	});
 });
