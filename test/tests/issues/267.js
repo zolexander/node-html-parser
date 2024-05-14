3c297dc178d0224ed6f89afd3ea9f451529dbb84
taoqf <tao_qiufeng@126.com>
2024-05-14 15:42:49 +0800

    add test for issue 267 #267

---
+++ test/tests/issues/267.js
@@ -0,0 +1,9 @@
+const { parse } = require('@test/test-target');
+
+describe.only('issue 267', function () {
+	it('Incorrect Handling of Empty Class Attributes in SVG Parsing', function () {
+		const html = `<polygon points="-235.18 1571.95 1014.73 1284.4 1083.46 1590.1 -166.45 1877.65 -235.18 1571.95" fill="#ff8200" class="" design-color="primary"></polygon> `;
+		const root = parse(html);
+		root.toString().should.eql(html);
+	});
+});
